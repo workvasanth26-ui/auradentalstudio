78e28fa40db475c41737f693ad8c4e8f9165ace3
workvasanth26-ui <work.vasanth26@gmail.com>
2026-08-28 11:14:03 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/auradentalstudio/",
   plugins: [
     tanstackStart({
       spa: {
