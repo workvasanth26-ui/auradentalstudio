ea8bf99f317d400f9f083b4a8cc89115130b6191
workvasanth26-ui <work.vasanth26@gmail.com>
2026-08-28 11:10:33 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from "@lovable/dev/vite-tanstack-config";
+import { defineConfig } from "vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [tailwindcss()],
-  tanstackStart: {
-    spa: {
-      enabled: true,
-    },
-  },
+  plugins: [
+    tanstackStart({
+      spa: {
+        enabled: true,
+      },
+    }),
+    tailwindcss(),
+  ],
 });
